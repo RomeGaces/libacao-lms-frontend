ae9e6db72a25a624f2e30fd813580c03e6a35f48
RoyMustang26 <jeromegaces02@gmail.com>
2025-12-01 18:15:17 +0800

    timeslot fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,12 @@ import staticRoutes from './static-routes'
 const router = createRouter({
   routes: [
     ...staticRoutes,
-    {
-      path: '/class-scheduling/timeslot/:day_of_week/:start_hour',
-      name: 'ScheduleTimeslot',
-      component: () => import('~/pages/scheduler/timeslot.vue'),
-      props: true,
-    },
+    // {
+    //   path: '/class-scheduling/timeslot/:day_of_week/:start_hour',
+    //   name: 'ScheduleTimeslot',
+    //   component: () => import('~/pages/scheduler/timeslot.vue'),
+    //   props: true,
+    // },
   ],
   history: createWebHistory(import.meta.env.VITE_APP_BASE),
 })
